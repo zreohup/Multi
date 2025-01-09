95d5711f7f3efcae81bd7f6591e32d534dd2395b
Usame Algan <5880855+usame-algan@users.noreply.github.com>
2025-01-09 09:47:15 +0100

    fix: Adjust meta tag to fix PWA header position (#4738)

--- apps/web/src/components/common/MetaTags/index.tsx
+++ apps/web/src/components/common/MetaTags/index.tsx
@@ -30,7 +30,7 @@ const MetaTags = ({ prefetchUrl }: { prefetchUrl: string }) => (
     <link rel="preconnect" href={prefetchUrl} crossOrigin="" />
 
     {/* Mobile tags */}
-    <meta name="viewport" content="width=device-width" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
 
     {/* PWA primary color and manifest */}
