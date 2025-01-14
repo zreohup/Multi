20490ea11839696189e0aa1c93b2b1fe8ebc8852
Jonathan Ferreira <jonathanferreira23@gmail.com>
2025-01-14 18:44:00 +0000

    Feat(Mobile): add logger and MMKV wrapper (#4756)
    * implements a wrapper on loggers
    
    * adds a mmkv utils
    
    * makes usage of enum instead
    
    * Update apps/mobile/src/store/constants.ts
    
    Co-authored-by: katspaugh <381895+katspaugh@users.noreply.github.com>
    
    * fix types
    
    * fix types usage on storage
    
    ---------
    
    Co-authored-by: katspaugh <381895+katspaugh@users.noreply.github.com>

--- apps/mobile/src/store/constants.ts
+++ apps/mobile/src/store/constants.ts
@@ -278,3 +278,47 @@ export const mockedChains = [
     transactionService: 'https://safe-transaction-arbitrum.safe.global',
   },
 ]
+
+export enum STORAGE_IDS {
+  SAFE = 'safe',
+  NOTIFICATIONS = 'notifications',
+  GLOBAL_PUSH_NOTIFICATION_SETTINGS = 'globalNotificationSettings',
+  SAFE_FCM_TOKEN = 'safeFcmToken',
+  PUSH_NOTIFICATIONS_PROMPT_COUNT = 'pushNotificationsPromptCount',
+  PUSH_NOTIFICATIONS_PROMPT_TIME = 'pushNotificationsPromptTime',
+  DEVICE_ID_STORAGE_KEY = 'pns=deviceId',
+  DEFAULT_NOTIFICATION_CHANNEL_ID = 'DEFAULT_NOTIFICATION_CHANNEL_ID',
+  ANNOUNCEMENT_NOTIFICATION_CHANNEL_ID = 'ANNOUNCEMENT_NOTIFICATION_CHANNEL_ID',
+  DEFAULT_PUSH_NOTIFICATION_CHANNEL_PRIORITY = 'high',
+  REQUEST_PERMISSION_ASKED = 'REQUEST_PERMISSION_ASKED',
+  REQUEST_PERMISSION_GRANTED = 'REQUEST_PERMISSION_GRANTED',
+  NOTIFICATION_DATE_FORMAT = 'DD/MM/YYYY HH:mm:ss',
+  NOTIFICATIONS_SETTINGS = 'notifications-settings',
+  PN_USER_STORAGE = 'safePnUserStorage',
+}
+
+export enum STORAGE_TYPES {
+  STRING = 'string',
+  BOOLEAN = 'boolean',
+  NUMBER = 'number',
+  OBJECT = 'object',
+}
+
+// Map all non string storage ids to their respective types
+export const mapStorageTypeToIds = (id: STORAGE_IDS): STORAGE_TYPES => {
+  switch (id) {
+    case STORAGE_IDS.NOTIFICATIONS:
+    case STORAGE_IDS.GLOBAL_PUSH_NOTIFICATION_SETTINGS:
+    case STORAGE_IDS.SAFE_FCM_TOKEN:
+    case STORAGE_IDS.NOTIFICATIONS_SETTINGS:
+    case STORAGE_IDS.PN_USER_STORAGE:
+      return STORAGE_TYPES.OBJECT
+    case STORAGE_IDS.PUSH_NOTIFICATIONS_PROMPT_COUNT:
+      return STORAGE_TYPES.NUMBER
+    case STORAGE_IDS.REQUEST_PERMISSION_ASKED:
+    case STORAGE_IDS.REQUEST_PERMISSION_GRANTED:
+      return STORAGE_TYPES.BOOLEAN
+    default:
+      return STORAGE_TYPES.STRING
+  }
+}
