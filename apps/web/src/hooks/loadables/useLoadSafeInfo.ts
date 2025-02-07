046aafaf5f9103138dd32d7f8e7835c87f389c51
katspaugh <381895+katspaugh@users.noreply.github.com>
2025-02-07 09:30:48 +0100

    Refactor(web): faster SafeInfo loading (#4883)

--- apps/web/src/hooks/loadables/useLoadSafeInfo.ts
+++ apps/web/src/hooks/loadables/useLoadSafeInfo.ts
@@ -1,7 +1,7 @@
 import { selectUndeployedSafe } from '@/features/counterfactual/store/undeployedSafesSlice'
 import { getUndeployedSafeInfo } from '@/features/counterfactual/utils'
 import { useAppSelector } from '@/store'
-import { useEffect } from 'react'
+import { useEffect, useMemo } from 'react'
 import { getSafeInfo, type SafeInfo } from '@safe-global/safe-gateway-typescript-sdk'
 import useAsync, { type AsyncResult } from '../useAsync'
 import useSafeAddress from '../useSafeAddress'
@@ -13,28 +13,29 @@ import { POLLING_INTERVAL } from '@/config/constants'
 import { useCurrentChain } from '../useChains'
 
 export const useLoadSafeInfo = (): AsyncResult<SafeInfo> => {
-  const address = useSafeAddress()
+  const address = useSafeAddress(true)
   const chainId = useChainId()
   const chain = useCurrentChain()
   const [pollCount, resetPolling] = useIntervalCounter(POLLING_INTERVAL)
   const { safe } = useSafeInfo()
   const isStoredSafeValid = safe.chainId === chainId && safe.address.value === address
+  const cache = isStoredSafeValid ? safe : undefined
   const undeployedSafe = useAppSelector((state) => selectUndeployedSafe(state, chainId, address))
 
-  const [data, error, loading] = useAsync<SafeInfo | undefined>(async () => {
-    if (!chainId || !address || !chain) return
-
+  const [undeployedData, undeployedError] = useAsync<SafeInfo | undefined>(async () => {
+    if (!undeployedSafe || !chain) return
     /**
      * This is the one place where we can't check for `safe.deployed` as we want to update that value
      * when the local storage is cleared, so we have to check undeployedSafe
      */
-    if (undeployedSafe) return getUndeployedSafeInfo(undeployedSafe, address, chain)
+    return getUndeployedSafeInfo(undeployedSafe, address, chain)
+  }, [undeployedSafe, address, chain])
 
+  const [cgwData, cgwError, cgwLoading] = useAsync<SafeInfo | undefined>(async () => {
+    if (!chainId || !address || pollCount === undefined) return
     const safeInfo = await getSafeInfo(chainId, address)
-
     return { ...safeInfo, deployed: true }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [chainId, address, pollCount, undeployedSafe, chain])
+  }, [chainId, address, pollCount])
 
   // Reset the counter when safe address/chainId changes
   useEffect(() => {
@@ -43,17 +44,17 @@ export const useLoadSafeInfo = (): AsyncResult<SafeInfo> => {
 
   // Log errors
   useEffect(() => {
-    if (error) {
-      logError(Errors._600, error.message)
+    if (cgwError) {
+      logError(Errors._600, cgwError.message)
     }
-  }, [error])
+  }, [cgwError])
 
-  return [
-    // Return stored SafeInfo between polls
-    data ?? (isStoredSafeValid ? safe : data),
-    error,
-    loading,
-  ]
+  // Return stored SafeInfo between polls
+  const safeData = cgwData ?? undeployedData ?? cache
+  const error = cgwError ?? (undeployedSafe ? undeployedError : undefined)
+  const loading = cgwLoading
+
+  return useMemo(() => [safeData, error, loading], [safeData, error, loading])
 }
 
 export default useLoadSafeInfo
--- apps/web/src/hooks/useSafeAddress.ts
+++ apps/web/src/hooks/useSafeAddress.ts
@@ -1,11 +1,24 @@
 import { useMemo } from 'react'
 import { useRouter } from 'next/router'
+import { parse, type ParsedUrlQuery } from 'querystring'
 import { parsePrefixedAddress } from '@/utils/addresses'
 
-const useSafeAddress = (): string => {
+// Use the location object directly because Next.js's router.query is available only on mount
+const getLocationQuery = (): ParsedUrlQuery => {
+  if (typeof location === 'undefined') return {}
+  const query = parse(location.search.slice(1))
+  return query
+}
+
+export const useUrlSafeAddress = (useLocation?: boolean): string | undefined => {
   const router = useRouter()
-  const { safe = '' } = router.query
-  const fullAddress = Array.isArray(safe) ? safe[0] : safe
+  const query = router.query && router.query?.safe ? router.query : useLocation ? getLocationQuery() : router.query
+  const safe = query.safe?.toString() || ''
+  return safe
+}
+
+const useSafeAddress = (useLocation?: boolean): string => {
+  const fullAddress = useUrlSafeAddress(useLocation)
 
   const checksummedAddress = useMemo(() => {
     if (!fullAddress) return ''
