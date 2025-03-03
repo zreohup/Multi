458e3d56ec7c02980b446641f405be2dda8eecdc
Chiranjeev Mishra <44394108+chiranjeev13@users.noreply.github.com>
2025-03-03 20:39:41 +0530

    fix(Safe Apps): keep appUrl query param when switching network (#5081)

--- apps/web/src/components/common/NetworkSelector/index.tsx
+++ apps/web/src/components/common/NetworkSelector/index.tsx
@@ -81,6 +81,7 @@ export const getNetworkLink = (router: NextRouter, safeAddress: string, networkS
     safe?: string
     chain?: string
     safeViewRedirectURL?: string
+    appUrl?: string
   }
 
   const route = {
@@ -90,6 +91,10 @@ export const getNetworkLink = (router: NextRouter, safeAddress: string, networkS
 
   if (router.query?.safeViewRedirectURL) {
     route.query.safeViewRedirectURL = router.query?.safeViewRedirectURL.toString()
+  }
+
+  if (router.query?.appUrl) {
+    route.query.appUrl = router.query.appUrl.toString()
   }
 
   return route
