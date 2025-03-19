deb462b3deff4d43bba3e872b59855ed82956275
Michael <30682308+mike10ca@users.noreply.github.com>
2025-03-19 11:35:18 +0100

    Tests: Add incoming tx decimal tests (#5369)

---
+++ apps/web/cypress/e2e/smoke/tx_history_filter_2.cy.js
@@ -0,0 +1,58 @@
+/* eslint-disable */
+import { getSafes, CATEGORIES } from '../../support/safes/safesHandler.js'
+import { buildQueryUrl } from '../../support/utils/txquery.js'
+import * as constants from '../../support/constants.js'
+
+let staticSafes = []
+let safeAddress
+
+const txType_incoming = 'incoming'
+
+describe('[SMOKE] API Tx history decimals filter tests', () => {
+  before(async () => {
+    staticSafes = await getSafes(CATEGORIES.static)
+  })
+
+  beforeEach(() => {
+    safeAddress = staticSafes.SEP_STATIC_SAFE_38.substring(4)
+  })
+
+  const chainId = constants.networkKeys.sepolia
+
+  // incoming tx
+  it('Verify incoming USDC can be filtered with decimals', () => {
+    const params = {
+      transactionType: txType_incoming,
+      value: '12087258546746105003',
+    }
+    const url = buildQueryUrl({ chainId, safeAddress, ...params })
+    console.log('*****. Generated URL:', url)
+
+    cy.request({
+      url: url,
+      failOnStatusCode: false,
+    }).then((response) => {
+      console.log(JSON.stringify(response.body))
+      expect(response.body).to.have.property('results').and.to.be.an('array')
+      expect(response.body.results).to.not.be.empty
+    })
+  })
+
+  it('Verify incoming ETH can be filtered with decimals', () => {
+    const params = {
+      transactionType: txType_incoming,
+      value: '50000000000000000',
+    }
+    const url = buildQueryUrl({ chainId, safeAddress, ...params })
+    console.log('*****. Generated URL:', url)
+
+    cy.request({
+      url: url,
+      failOnStatusCode: false,
+    }).then((response) => {
+      console.log(JSON.stringify(response.body))
+      expect(response.body).to.have.property('results').and.to.be.an('array')
+      expect(response.body.results).to.not.be.empty
+    })
+  })
+})
