3bd13bf1cc0094d9956646d919891827c84822ad
Michael <30682308+mike10ca@users.noreply.github.com>
2025-03-24 12:49:05 +0100

    Tests: Fix tests (#5482)
    * Fix tests

--- apps/web/cypress/e2e/regression/twaps_history.cy.js
+++ apps/web/cypress/e2e/regression/twaps_history.cy.js
@@ -38,6 +38,7 @@ describe('Twaps history tests', { defaultCommandTimeout: 30000 }, () => {
       swaps.selectInputCurrency(swaps.swapTokens.cow)
       swaps.setInputValue(500)
       swaps.selectOutputCurrency(swaps.swapTokens.dai)
+      swaps.confirmPriceImpact()
       swaps.verifyReviewOrderBtnIsVisible()
       swaps.getTwapInitialData().then((formData) => {
         cy.wrap(formData).as('twapFormData')
