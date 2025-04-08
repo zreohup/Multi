1dbf0765b7e9a259c891fda0cc72728a05fa4412
katspaugh <381895+katspaugh@users.noreply.github.com>
2025-04-08 14:06:48 +0200

    Chore(web): fix prod deployment workflow (#5639)

--- apps/web/scripts/github/s3_upload.sh
+++ apps/web/scripts/github/s3_upload.sh
@@ -9,18 +9,31 @@ fi
 cd out
 
 # Upload the build to S3
-aws s3 sync . "$BUCKET" --delete
+aws s3 sync . $BUCKET --delete
+
+function parallel_limit {
+    local max="$1"
+    while (( $(jobs -rp | wc -l) >= max )); do
+        sleep 0.1
+    done
+}
 
 export BUCKET
 
 MAX_JOBS=10
 
-# Upload all HTML files again but w/o an extension so that URLs like /welcome open the right page
-find . -name '*.html' -print0 | \
-xargs -0 -n 1 -P "$MAX_JOBS" -I {} bash -c '
-  filepath="{}"
-  noext="${filepath%.html}"
-  aws s3 cp "$filepath" "$BUCKET/$noext" --content-type "text/html"
-'
+# Upload all HTML files again but w/o an extention so that URLs like /welcome open the right page
+find . -name '*.html' -print0 | while IFS= read -r -d '' file; do
+    filepath="${file#./}"
+    noext="${filepath%.html}"
+
+    # Throttle jobs when max limit is hit
+    parallel_limit "$MAX_JOBS"
+
+    # Upload files to S3 using parallel threads
+    aws s3 cp "$filepath" "$BUCKET/$noext" --content-type 'text/html' &
+done
+
+wait
 
 cd -
