329219e31b534698a686c7fdb4db7007dd12c911
Clóvis Neto <clovisdasilvaneto@gmail.com>
2025-04-21 10:24:52 +0200

    fix: put the current theme in a local state to avoid side effects of useColorScheme hook (#5727)

--- apps/mobile/src/theme/hooks/useTheme.tsx
+++ apps/mobile/src/theme/hooks/useTheme.tsx
@@ -1,5 +1,5 @@
-import { useCallback, useMemo } from 'react'
-import { useColorScheme } from 'react-native'
+import { useCallback, useEffect, useState } from 'react'
+import { AppState, ColorSchemeName, useColorScheme } from 'react-native'
 import { updateSettings } from '@/src/store/settingsSlice'
 import { selectSettings } from '@/src/store/settingsSlice'
 import { useAppDispatch, useAppSelector } from '@/src/store/hooks'
@@ -8,6 +8,8 @@ import { ThemePreference } from '@/src/types/theme'
 export const useTheme = () => {
   const dispatch = useAppDispatch()
   const colorScheme = useColorScheme()
+  const [currentTheme, setTheme] = useState<ColorSchemeName>(colorScheme)
+
   const themePreference = useAppSelector(
     (state) => selectSettings(state, 'themePreference') ?? 'auto',
   ) as ThemePreference
@@ -19,9 +21,20 @@ export const useTheme = () => {
     [dispatch],
   )
 
-  const currentTheme = useMemo(() => {
-    return themePreference === 'auto' ? colorScheme : themePreference
-  }, [themePreference, colorScheme])
+  // Prevent theme from changing when the app is in the background
+  // Issue: https://github.com/facebook/react-native/issues/35972
+  // They closed but issue still exists. Check the link for more details.
+  useEffect(() => {
+    const subscription = AppState.addEventListener('change', (nextAppState) => {
+      if (nextAppState === 'active') {
+        setTheme(themePreference === 'auto' ? colorScheme : themePreference)
+      }
+    })
+
+    return () => {
+      subscription.remove()
+    }
+  }, [colorScheme, themePreference])
 
   return { themePreference, setThemePreference, currentTheme }
 }
