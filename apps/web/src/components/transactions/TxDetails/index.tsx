0945d57efded804ca176a5c36c5d6eb23bec4c62
katspaugh <381895+katspaugh@users.noreply.github.com>
2025-04-22 15:56:57 +0200

    Refactor(Proposers): use proposedByDelegate (#5740)

--- apps/web/src/components/transactions/TxDetails/index.tsx
+++ apps/web/src/components/transactions/TxDetails/index.tsx
@@ -54,11 +54,6 @@ const TxDetailsBlock = ({ txSummary, txDetails }: TxDetailsProps): ReactElement 
   const isUnsigned =
     isMultisigExecutionInfo(txSummary.executionInfo) && txSummary.executionInfo.confirmationsSubmitted === 0
 
-  const isTxFromProposer =
-    isMultisigDetailedExecutionInfo(txDetails.detailedExecutionInfo) &&
-    txDetails.detailedExecutionInfo.trusted &&
-    isUnsigned
-
   const isUntrusted =
     isMultisigDetailedExecutionInfo(txDetails.detailedExecutionInfo) && !txDetails.detailedExecutionInfo.trusted
 
@@ -69,7 +64,6 @@ const TxDetailsBlock = ({ txSummary, txDetails }: TxDetailsProps): ReactElement 
   let proposer, safeTxHash, proposedByDelegate
   if (isMultisigDetailedExecutionInfo(txDetails.detailedExecutionInfo)) {
     safeTxHash = txDetails.detailedExecutionInfo.safeTxHash
-    // @ts-expect-error TODO: Need to update the types from the new SDK
     proposedByDelegate = txDetails.detailedExecutionInfo.proposedByDelegate
     proposer = proposedByDelegate?.value ?? txDetails.detailedExecutionInfo.proposer?.value
   }
@@ -135,13 +129,13 @@ const TxDetailsBlock = ({ txSummary, txDetails }: TxDetailsProps): ReactElement 
         )}
       </div>
       {/* Signers */}
-      {(!isUnsigned || isTxFromProposer) && (
+      {(!isUnsigned || proposedByDelegate) && (
         <div className={css.txSigners}>
           <TxSigners
             txDetails={txDetails}
             txSummary={txSummary}
-            isTxFromProposer={isTxFromProposer}
-            proposer={proposedByDelegate}
+            isTxFromProposer={Boolean(proposedByDelegate)}
+            proposer={proposer}
           />
 
           <TxShareBlock txId={txDetails.txId} txHash={txDetails.txHash} />
