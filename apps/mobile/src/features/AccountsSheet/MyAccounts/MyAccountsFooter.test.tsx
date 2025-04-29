21350059de87411cf729293c2b5f7cb083da211e
Clóvis Neto <clovisdasilvaneto@gmail.com>
2025-04-29 12:46:18 +0200

    Fix(mobile): Remove "Join Account" button from account sheet (#5773)
    * fix: remove join account button
    
    * fix: unit tests

--- apps/mobile/src/features/AccountsSheet/MyAccounts/MyAccountsFooter.test.tsx
+++ apps/mobile/src/features/AccountsSheet/MyAccounts/MyAccountsFooter.test.tsx
@@ -6,6 +6,5 @@ describe('MyAccountsFooter', () => {
     const container = render(<MyAccountsFooter />)
 
     expect(container.getByText('Add existing account')).toBeDefined()
-    expect(container.getByText('Join new account')).toBeDefined()
   })
 })
