62bb24a3e203c5bac64bf3c0156130504b8f3568
Daniel Dimitrov <daniel.d@safe.global>
2025-04-30 15:06:33 +0200

    fix(mobile): link „why did it happen“ article

--- apps/mobile/src/features/ConflictTxSheet/ConflictTxSheet.container.tsx
+++ apps/mobile/src/features/ConflictTxSheet/ConflictTxSheet.container.tsx
@@ -1,6 +1,8 @@
 import { Badge } from '@/src/components/Badge'
 import { SafeBottomSheet } from '@/src/components/SafeBottomSheet'
 import { SafeFontIcon } from '@/src/components/SafeFontIcon'
+import { Link } from 'expo-router'
+import { HelpCenterArticle } from '@safe-global/utils/config/constants'
 import { H3, H6, Text, View } from 'tamagui'
 
 export const ConflictTxSheetContainer = () => {
@@ -22,12 +24,22 @@ export const ConflictTxSheetContainer = () => {
           the other(s).
         </H6>
 
-        <View marginTop="$4" flexDirection="row" alignItems="center" gap="$2">
-          <SafeFontIcon color="$textSecondaryLight" name="info" size={16} />
-          <Text fontWeight={600} color="$textSecondaryLight">
-            Why did it happen?
-          </Text>
-        </View>
+        <Link href={HelpCenterArticle.CONFLICTING_TRANSACTIONS} asChild>
+          <View
+            marginTop="$2"
+            flexDirection="row"
+            justifyContent="center"
+            alignItems="center"
+            gap="$2"
+            paddingVertical="$3"
+            paddingHorizontal="$10"
+          >
+            <SafeFontIcon color="$textSecondaryLight" name="info" size={16} />
+            <Text fontWeight={600} color="$textSecondaryLight">
+              Why did it happen?
+            </Text>
+          </View>
+        </Link>
       </View>
     </SafeBottomSheet>
   )
