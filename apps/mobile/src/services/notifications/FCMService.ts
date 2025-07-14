b2176b7bdbdbcc1381bf3636074910c1208f23fa
Daniel Dimitrov <daniel.d@safe.global>
2025-07-14 14:29:46 +0200

    fix(mobile): register earlier for apns

--- apps/mobile/src/services/notifications/FCMService.ts
+++ apps/mobile/src/services/notifications/FCMService.ts
@@ -1,6 +1,5 @@
 import { getMessaging } from '@react-native-firebase/messaging'
 import Logger from '@/src/utils/logger'
-import { withTimeout } from '@/src/utils/notifications'
 import { savePushToken } from '@/src/store/notificationsSlice'
 import { getStore } from '@/src/store/utils/singletonStore'
 
@@ -16,9 +15,7 @@ class FCMService {
 
   async saveFCMToken(): Promise<void> {
     try {
-      // Register the app with FCM forcefully to get the token since it has not been reliably saved otherwise
-      await getMessaging().registerDeviceForRemoteMessages()
-      const fcmToken = await withTimeout(getMessaging().getToken(), 10000)
+      const fcmToken = await getMessaging().getToken()
       Logger.info('FCMService :: fcmToken', fcmToken)
       if (fcmToken) {
         getStore().dispatch(savePushToken(fcmToken))
@@ -28,20 +25,8 @@ class FCMService {
     }
   }
 
-  async registerAppWithFCM(): Promise<void> {
-    await getMessaging()
-      .registerDeviceForRemoteMessages()
-      .then((status: unknown) => {
-        Logger.info('registerDeviceForRemoteMessages status', status)
-      })
-      .catch((error) => {
-        Logger.error('registerAppWithFCM: Something went wrong', error)
-      })
-  }
-
   async initNotification(): Promise<string | undefined> {
     try {
-      await this.registerAppWithFCM()
       await this.saveFCMToken()
       const fcmToken = await this.getFCMToken()
       return fcmToken
